f8fb225c47131c28b8974206f695d4dd70e61b8b
Harish Chobisa <81731230+harishgchobisa@users.noreply.github.com>
2025-06-05 20:48:34 +0530

    footer Implementation

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,13 +1,57 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, } from 'react-bootstrap';
+import { NavLink } from 'react-router';
+import { SiGooglemaps } from "react-icons/si";
+import { IoIosMail } from "react-icons/io";
+import { FiPhoneCall } from "react-icons/fi";
+import footerInfo from '../../../services/footerInfo.json';
+
+const icons = {
+  IoIosMail: <IoIosMail />,
+  FiPhoneCall: <FiPhoneCall />,
+  SiGooglemaps: <SiGooglemaps />
+};
 
 import './footer.scss';
 
 const Footer = () => {
   return(
     <footer>
-      <Container>
-        Footer
-      </Container>
+      <div className="footer-info">
+        <Container>
+          <Row>
+            {
+              footerInfo.map((item, index) => {
+                return (
+                  <Col sm={4} className="footer-info__item" key={index}>
+                    {icons[item.iconName]}
+                    <div className="footer-info__item__content">
+                      <h6>{item.title}</h6>
+                      <p>{item.content}</p>
+                    </div>
+                  </Col>
+                )
+              })
+            }
+          </Row>
+        </Container>
+      </div>
+      <div className="footer-copyright">
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <p>&copy; {new Date().getFullYear()} Danza. All rights reserved.</p>
+            </Col>
+            <Col sm={6} className="links">
+              <ul>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/products">Products</NavLink></li>
+                <li><NavLink to="/blogs">Blogs</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </footer>
   )
 }
